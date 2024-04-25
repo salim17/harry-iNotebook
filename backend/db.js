5077972cccef6ad9f34542bb79ca5d5f41bd9336
Mohd Salim <mohd.salim@ibm.com>
2024-04-25 23:30:43 +0530

    video 43 now

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const mongoURI = "mongodb://127.0.0.1:27017/?readPreference=primary&appname=Mong
 /** If you add await in an async method the execution waits where ever you use await. so at line 7 it will wait for the promise to resolve */
 const connectToMongo = async () => {
     await mongoose.connect(mongoURI).then(() => console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+    console.log("even though i am inside async function, i will wait for await...  ")
 }
 
 module.exports = connectToMongo;
