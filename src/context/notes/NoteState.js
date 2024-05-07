65f07d8274a7b14924f8d8554af7ee4fcecc9874
Mohd Salim <mohd.salim@ibm.com>
2024-05-08 02:32:04 +0530

    video 58 create context intro

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+  const s1 = {
+    name: "Harry",
+    class: "5b",
+  };
+  const [state, setState] = useState(s1);
+  const update = () => {
+    setTimeout(() => {
+        setState( {
+            "name" : "Larry",
+            "class" : "10b"
+        })
+    }, 1000);
+  }
+  return (
+    <NoteContext.Provider value={{state, update}}>{props.children}</NoteContext.Provider>
+  );
+};
+
+export default NoteState;
